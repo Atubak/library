bb9913c43a85cc65862cb5e093064cc60cbab3ea
Jona <mr-jona@hotmail.com>
2021-10-07 16:29:46 +0200

    add function to display the books on the webpage

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+const bookSection = document.querySelector('.bookSection');
+
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -6,7 +8,7 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.info = function() {
-        return `${title} by ${author} with a length of ${pages}. Have ${read}.`
+        return `${title} by ${author} with a length of ${pages} pages. Have ${read}.`
     };
 };
 
@@ -23,3 +25,25 @@ addBookToLibrary('Harry Potter', 'J.K. Rowling', '4050', 'not read yet');
 addBookToLibrary('Dune', 'Frank Herbert', '400', 'read');
 addBookToLibrary('Hunger Games', 'Idontevenknow', '6969', 'not read');
 addBookToLibrary('Dune Messiah', 'Frank Herbert', '420', 'not read');
+
+
+
+function displayBookOnPage() {
+    
+    myLibrary.forEach(function(book) {
+        const div = document.createElement('div');
+        div.classList.add('book');
+
+        const header = document.createElement('h3');
+        header.textContent = `${book.title}`;
+        div.appendChild(header);
+
+        const p = document.createElement('p');
+        p.textContent = `${book.info()}`;
+        div.appendChild(p);
+        
+        bookSection.appendChild(div);
+    });
+};
+
+displayBookOnPage();
