f64e5227ee7760f92dc5547051cb6a36d1c68b57
Jona <mr-jona@hotmail.com>
2021-10-08 14:25:45 +0200

    add a remove button and add a read toggle button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 const bookSection = document.querySelector('.bookSection');
 const newBookButton = document.querySelector('.newBook');
-let newBookForm = document.querySelector('p');
-let submitButton = document.querySelector('p');
-
+let newBookForm;
+let submitButton;
+let removeButton;
+let divRead;
 
 
 
@@ -13,7 +14,11 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.info = function() {
-        return `${title} by ${author} with a length of ${pages} pages. Have ${read}.`
+        return `${title} by ${author} with a length of ${pages} pages.`
+    };
+    this.toggleRead = function() {
+        if (this.read === 'Read') return this.read = 'Not Read Yet';
+        if (this.read === 'Not Read Yet') return this.read = 'Read';
     };
 };
 
@@ -29,46 +34,57 @@ function addBookToLibrary(title, author, pages, read) {
 
 
 
-// addBookToLibrary('Harry Potter', 'J.K. Rowling', '4050', 'not read yet');
-// addBookToLibrary('Dune', 'Frank Herbert', '400', 'read');
-// addBookToLibrary('Hunger Games', 'Idontevenknow', '6969', 'not read yet');
-// addBookToLibrary('Dune Messiah', 'Frank Herbert', '420', 'not read yet');
 
 
-// call to display the array on the page
+// call to display the array objects on the page
 function displayBooksOnPage() {
     // delete all old books first because it will add everything on top of the existing books!!on the page!! (the array is fine)
     let booksToDelete = document.querySelectorAll('.book');
     booksToDelete.forEach(function(book) {
         book.remove();
     });
+    
 
-
-// then add them again with the newly added book included
+    // then add them again with the newly added book included
     myLibrary.forEach(function(book) {
         const div = document.createElement('div');
         div.classList.add('book');
+        div.setAttribute('data-bookObjectIndex', `${myLibrary.indexOf(book)}`);
+
+        removeButton = document.createElement('button');
+        removeButton.classList.add('removeButton');
+        removeButton.textContent = 'X';
+        removeButtonListener(); //allows the remove button listener to only start listening after the button is made on the page
+        div.appendChild(removeButton);
 
         const header = document.createElement('h3');
         header.textContent = `${book.title}`;
         div.appendChild(header);
+        
+        const pInfo = document.createElement('p');
+        pInfo.textContent = `${book.info()}`;
+        div.appendChild(pInfo);
 
-        const p = document.createElement('p');
-        p.textContent = `${book.info()}`;
-        div.appendChild(p);
+        divRead = document.createElement('div');
+        divRead.classList.add('divRead');
+        divRead.textContent = (`${book.read}` === 'Read') ? `✅ ${book.read}`:`❌ ${book.read}`;
+        divRead.style['background-color'] = (`${book.read}` === 'Read') ? 'green':'orangered';
+        readButtonListener(); //allows the read button listener to only start listening after the button is made on the page
+        div.appendChild(divRead);
         
         bookSection.insertBefore(div, newBookButton);
     });
+
 };
 
 
 // Making a form appear after clicking the new-book-button
 newBookButton.addEventListener('click', () => {
     bookSection.removeChild(newBookButton);
-
+    
     newBookForm = document.createElement('div');
     newBookForm.classList.add('newBookForm');
-
+    
     const makeForm = document.createElement('form');
     makeForm.innerHTML = `<label for="title">Title:</label><br>
     <input type="text" id="title" name="title"><br>
@@ -77,15 +93,15 @@ newBookButton.addEventListener('click', () => {
     <label for="pages">Number of Pages:</label><br>
     <input type="number" id="pages" name="pages"><br>
     <p>Have You Read It Already?</p>
-    <input type= "radio" name="read" id="notread" value="not read yet">
+    <input type= "radio" name="read" id="notread" value="Not Read Yet">
     <label for="notread">Not Yet</label><br>
-    <input type="radio" name="read" id="read" value="read">
+    <input type="radio" name="read" id="read" value="Read">
     <label for="read">Yep!</label><br>
     <button type="button" id="submitButton">Submit</button>`;
-
-
+    
+    
     newBookForm.appendChild(makeForm);
-
+    
     bookSection.appendChild(newBookForm);
     submitButton = document.querySelector('#submitButton');
     submitButtonListener(); //allows the submit event listener to only start listening after the form is made
@@ -96,18 +112,55 @@ newBookButton.addEventListener('click', () => {
 
 // taking the info from the form and inserting it in the addBookToLibrary function
 function submitButtonListener() {
-submitButton.addEventListener('click', () => {
-    let title = document.querySelector('#title').value;
-    let author = document.querySelector('#author').value;
-    let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('input[name="read"]:checked').value;
+    submitButton.addEventListener('click', () => {
+        let title = document.querySelector('#title').value;
+        let author = document.querySelector('#author').value;
+        let pages = document.querySelector('#pages').value;
+        let read = document.querySelector('input[name="read"]:checked').value;
+        
+        bookSection.removeChild(newBookForm);
+        
+        bookSection.appendChild(newBookButton);
+        addBookToLibrary(title, author, pages, read);
+        displayBooksOnPage();
+    });};
     
-    bookSection.removeChild(newBookForm);
-    
-    bookSection.appendChild(newBookButton);
-    addBookToLibrary(title, author, pages, read);
-    displayBooksOnPage();
-});};
+
+ 
+function removeButtonListener() {    
+    removeButton.addEventListener('click', (event) => {
+        let bookToBeRemoved = event.path[1];
+        let bookToBeRemovedObjectIndex = bookToBeRemoved.getAttribute('data-bookObjectIndex');
+
+        if (confirm('This action will delete the book from your library.')) {
+            bookSection.removeChild(bookToBeRemoved);
+            myLibrary.splice(bookToBeRemovedObjectIndex, 1);
+        } else return;
+
+    });
+};    
 
 
-// MOET KIJKEN WAAROM DE LIBRARY DE OUDE BOEKEN NOG EEN KEER WEERGEEFT
+
+function readButtonListener() {
+    divRead.addEventListener('click', (event) => {
+        let bookToBeToggledObjectIndex = event.path[1].getAttribute('data-bookObjectIndex');
+        myLibrary[bookToBeToggledObjectIndex].toggleRead();
+        
+        return displayBooksOnPage(); //to refresh the page so that the button updates color etc
+    })
+};
+
+
+
+
+
+// Sample books
+    addBookToLibrary('Harry Potter', 'J.K. Rowling', '4050', 'Not Read Yet');
+    addBookToLibrary('Dune', 'Frank Herbert', '400', 'Read');
+    addBookToLibrary('Hunger Games', 'Idontevenknow', '6969', 'Not Read Yet');
+    addBookToLibrary('Dune Messiah', 'Frank Herbert', '420', 'Not Read Yet');
+
+
+
+displayBooksOnPage();
